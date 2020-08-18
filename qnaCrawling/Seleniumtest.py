375d7a679188a4a937c41f0588e68db7a740be67
kerow1004@gmail.com <kerow1004@gmail.com>
2020-08-18 21:13:05 +0900

    코드 수정

--- qnaCrawling/Seleniumtest.py
+++ qnaCrawling/Seleniumtest.py
@@ -1,14 +1,63 @@
-import os
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
+from urllib.request import urlopen
+from bs4 import BeautifulSoup as bs
+
+ebsUrl = "https://mid.ebs.co.kr/teacher/middle/index?searchSiteCode=all&subjectCd=32000003&searchKeyword=&isSearch=Y"
 
 ff_driver = webdriver.Chrome('/Users/jinseongkim/chromedriver')
-ff_driver.get("http://www.g2b.go.kr/pt/menu/selectSubFrame.do?framesrc=/pt/menu/frameTgong.do?url=http://www.g2b.go.kr:8101/ep/tbid/tbidFwd.do")
+ff_driver.implicitly_wait(3)
+ff_driver.get(ebsUrl)
 
-query = ff_driver.find_element_by_name("prodNm").send_keys("범죄")
-ff_driver.find_element_by_name("btnK").click()
+qnaList = []
+
+# Q&A페이지 이동
+def pageMove(teacherNo):
+    ff_driver.find_element_by_id("teacher_box_"+teacherNo).click()
+
+    ff_driver.find_element_by_xpath("""//*[@id="container"]/div[4]/div/div/div[1]/ul/li[3]/a""").click()
+
+    # 총페이지 리스트에 담기
+    pages = ff_driver.find_element_by_class_name("page_wrap").text
+    pageList = []
+
+    for page in pages:
+        pageList.append(page)
+
+    for page in pageList:
+        ff_driver.find_element_by_xpath("""//*[@id="container"]/div[4]/div/div/div[2]/div[5]/span/a[""" + page + """]""").click()
+
+
+        # 각 페이지별 클로링
+        for i in range(1, 21):
+            try:
+                ff_driver.implicitly_wait(3)
+                qnaData = ff_driver.find_element_by_xpath("""//*[@id="container"]/div[4]/div/div/div[2]/form[2]/table/tbody/tr["""+ str(i) +"""]/td/div[2]/div[1]/div""").text
+                print(qnaData)
+                qnaList.append(qnaData)
+            except:
+                pass
+
+
+
+
+
+if __name__ == "__main__":
+    bsObject = bs(urlopen(ebsUrl), "html.parser")
+    teacherNos = bsObject.find_all("div", {"class": "teacher_box"})
+    # 선생님페이지로 이동
+    for teacherNo in range(len(teacherNos)):
+        pageMove(teacherNo)
+        ff_driver.close()
+
+
+
+
+
+# print(pageList)
+
+
+# print(ff_driver.current_url)
+# ff_driver.find_element_by_name("btnK").click()
 
 # WebDriverWait(ff_driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "h3.r")))
 #
@@ -16,3 +65,12 @@ ff_driver.find_element_by_name("btnK").click()
 #
 # for item in page_results:
 #     print(item.text)
+
+#
+# //*[@id="container"]/div[4]/div/div/div[2]/div[5]/span/a[2]
+# //*[@id="container"]/div[4]/div/div/div[2]/div[5]/span/a[3]
+# //*[@id="container"]/div[4]/div/div/div[2]/div[5]/span/a[2]
+# //*[@id="container"]/div[4]/div/div/div[2]/div[5]/span/a[1]
+# //*[@id="container"]/div[4]/div/div/div[2]/form[2]/table/tbody/tr[1]/td/div[2]/div[1]/div
+# //*[@id="container"]/div[4]/div/div/div[2]/form[2]/table/tbody/tr[2]/td/div[2]/div[1]/div
+# //*[@id="container"]/div[4]/div/div/div[2]/form[2]/table/tbody/tr[3]/td/div[2]/div[1]/div
--- qnaCrawling/ebsCrawling.py
+++ qnaCrawling/ebsCrawling.py
@@ -1,24 +1,56 @@
+from selenium import webdriver
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as bs
 
+ebsUrl = "https://mid.ebs.co.kr/teacher/middle/index?searchSiteCode=all&subjectCd=32000003&searchKeyword=&isSearch=Y"
+
+
+
+qnaList = []
+
+# Q&A페이지 이동
+def pageMove(teacherNo):
+    ff_driver.find_element_by_id("teacher_box_"+teacherNo).click()
+
+    ff_driver.find_element_by_xpath("""//*[@id="container"]/div[4]/div/div/div[1]/ul/li[3]/a""").click()
+
+    # 총페이지 리스트에 담기
+    pages = ff_driver.find_element_by_class_name("page_wrap").text
+    pageList = []
+
+    for page in pages:
+        pageList.append(page)
+
+    for page in pageList:
+        ff_driver.find_element_by_xpath("""//*[@id="container"]/div[4]/div/div/div[2]/div[5]/span/a[""" + page + """]""").click()
+
+
+        # 각 페이지별 클로링
+        for i in range(1, 21):
+            try:
+                ff_driver.implicitly_wait(3)
+                qnaData = ff_driver.find_element_by_xpath("""//*[@id="container"]/div[4]/div/div/div[2]/form[2]/table/tbody/tr["""+ str(i) +"""]/td/div[2]/div[1]/div""").text
+                print(qnaData)
+                qnaList.append(qnaData)
+            except:
+                pass
+
+
+
+
+
 if __name__ == "__main__":
-    url = urlopen("https://mid.ebs.co.kr/teacher/middle/index?searchSiteCode=all&subjectCd=32000003&searchKeyword=&isSearch=Y")
-    bsObject = bs(url, "html.parser")
+    bsObject = bs(urlopen(ebsUrl), "html.parser")
+    teacherNos = bsObject.find_all("div", {"class": "teacher_box"})
+    # 선생님페이지로 이동
+    for teacherNo in range(len(teacherNos)):
+        ff_driver = webdriver.Chrome('/Users/jinseongkim/chromedriver')
+        ff_driver.implicitly_wait(3)
+        ff_driver.get(ebsUrl)
 
-    # a = bsObject.get("teacherid")
-    # print(a)
-    teacherIdLists = []
-    teacherIds = bsObject.find_all("div", {"class":"teacher_box"})
-    # teacherId = bsObject.find_all("div", {"class":"teacher_box"})
-    for teacherId in teacherIds:
-        teacherIdLists.append(teacherId["teacherid"])
+        pageMove(str(teacherNo))
+        # ff_driver.close()
 
-    for teacherIdlist in teacherIdLists:
-        suburl = urlopen("https://mid.ebs.co.kr/teacher/view?teacherId="+teacherIdlist+"&subjectCd=32000003#qna/list")
-        teacherQnAPage = bs(suburl, "html.parser")
-
-        qnaTxt = teacherQnAPage.find("div", {"class":"question_inner"})
-
-        print()
+    print(qnaList)
 
 
