4a2da4333d1693a0bba565fe9f69c07276872bc6
kerow1004@gmail.com <kerow1004@gmail.com>
2020-08-18 17:38:41 +0900

    환경변화

--- yes24crawling/venv/lib/python3.7/site-packages/hml_equation_parser/__init__.py
+++
@@ -1,4 +0,0 @@
-from .hulkEqParser import hmlEquation2latex as eq2latex
-from .hmlParser import parseHml as parseHmlSample
-from .hmlParser import convertEquation as convertEquationSample
-from .hmlParser import extract2HtmlStr as extract2HtmlStrSample
